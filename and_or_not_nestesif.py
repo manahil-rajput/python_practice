69ec25d87aee70b5086c53e4fcfb260b2cc65c2f
Manahil Mudaser <manahilmudaser@gmail.com>
2024-08-09 15:52:25 +0500

    Add files via upload

---
+++ and_or_not_nestesif.py
@@ -0,0 +1,41 @@
+
+# And:
+# Test if a is greater than b, AND if c is greater than a:
+# a = 200
+# b = 33
+# c = 500
+# if a > b and c > a:
+#   print("Both conditions are True")
+
+# Or:
+# Test if a is greater than b, OR if a is greater than c:
+# a = 200
+# b = 33
+# c = 500
+# if a > b or a > c:
+#     print("At least one of the conditions is True")
+
+# Not:
+# Test if a is NOT greater than b:
+# a = 33
+# b = 200
+# if not a > b:
+#     print("a in Not greater than b")
+
+
+# Nested If:
+# x = 41
+# if x > 10:
+#     print("Above ten,")
+#     if x > 20:
+#         print("and also above 20!")
+#     else:
+#         print("but not above 20.")
+
+
+# The pass statement:
+a = 33
+b = 200
+if b < a:
+    pass 
+# kisi b error ko rokny ky liye hm pass use karty hain ky wo error show na kary.
