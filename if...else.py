0ff6c49c9783a7f43612c6fe16eea157d8603fdd
Manahil Mudaser <manahilmudaser@gmail.com>
2024-08-09 13:09:26 +0500

    Add files via upload

---
+++ if...else.py
@@ -0,0 +1,59 @@
+# if statement
+# a = 3
+# b = 5
+# if b > a:
+#     print("b is greater than a")
+
+# Indentation:
+# a = 3
+# b = 5
+# if b > a:
+# print("b is greater than a")
+# print loop sy bahir hai us ko loop krna or abi o error dy ga lein jb hm us ko loop main karin gy to 
+# wo error nai dy ga :
+
+#  if, elif:
+# a = 4
+# b = 4
+# if a > b:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+
+# if, elif, else:
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+
+# if, else:
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
+
+# short hand if:
+# one line if statement:
+# a = 200
+# b = 199
+# if a > b:  print("a is greater than b")
+
+# short hand if....else
+# one line if else statement:
+# a = 2
+# b = 33
+# # print("A") if a > b else print("B") in dono main sy koi b likh sakty hin answer B  yan small b hi aye ga.
+# print("a") if a > b else print("b")
+
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a == b else print("B").
+
+
